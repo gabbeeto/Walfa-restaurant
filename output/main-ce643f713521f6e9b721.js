bbd9df5eaa359dbad691c858bb1a8cc84a437f16
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-08-19 15:41:51 -0300

    fix few problems

---
+++ output/main-ce643f713521f6e9b721.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./source/html.js ***!
+  \************************/
+alert('this works');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi1jZTY0M2Y3MTM1MjFmNmU5YjcyMS5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2FsZmEtcmVzdGF1cmFudC8uL3NvdXJjZS9odG1sLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImFsZXJ0KCd0aGlzIHdvcmtzJyk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,14 @@ module.exports = {
 	mode: 'development',
 	devtool: 'inline-source-map',
 	plugins: [ new html({title:'Walfa Restaurant', template: './source/index.html' })],
-	entry: './source/main.js',
+	entry:{ htmlContent: './source/html.js',
+	functionality: './source/main.js'} ,
 	output:{ filename: 'main-[contenthash].js',
 		path: path.resolve(__dirname, 'output'),
 		clean: true,
 		assetModuleFilename: '[name][ext]'
 	},
 	module: {
-		rule:[{test: /\.css$/, use: ['style-loader', 'css-loader']},{test: /\.(png|ttf|otf)$/, type: 'asset/resource'}] 
+		rules:[{test: /\.css$/, use: ['style-loader', 'css-loader']},{test: /\.(png|ttf|otf)$/, type: 'asset/resource'}] 
 	}
 }
